c109fabb75e6255126586374036ee146b2c6cfa2
Darren Soulier <56926037+Kichuwa@users.noreply.github.com>
2024-05-09 21:50:17 -0500

    Added data filters

--- backend.py
+++ backend.py
@@ -5,12 +5,20 @@ import requests
 load_dotenv(".env")
 KEY = os.getenv("API_KEY")
 
+
 def get_data(place, days=None, option=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={KEY}"
     response = requests.get(url)
     data = response.json()
-    return data
+    filtered_data = data["list"]
+    date_value = 8 * days
+    filtered_data = filtered_data[:date_value]
+    if option == "Temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    if option == "Sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo"))
+    print(get_data(place="Tokyo", days=3, option="Temperature"))
