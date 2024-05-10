28b8a1b3c46e92a4d021fbfcf388ab58dd5a7bbc
Darren Soulier <56926037+Kichuwa@users.noreply.github.com>
2024-05-09 22:23:12 -0500

    Shifted condition part of method to main.py

--- backend.py
+++ backend.py
@@ -6,19 +6,15 @@ load_dotenv(".env")
 KEY = os.getenv("API_KEY")
 
 
-def get_data(place, days=None, option=None):
+def get_data(place, days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     date_value = 8 * days
     filtered_data = filtered_data[:date_value]
-    if option == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if option == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo", days=3, option="Temperature"))
+    print(get_data(place="Tokyo", days=3))
