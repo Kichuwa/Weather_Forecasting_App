4c8d89458993db087ad09f3cf4507971540d41f8
Darren Soulier <56926037+Kichuwa@users.noreply.github.com>
2024-05-09 22:23:34 -0500

    Added data passing to front end for use

--- main.py
+++ main.py
@@ -10,9 +10,26 @@ option = st.selectbox("Select Data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-d, t = get_data(place, days, option)
+if place:
 
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+    try:
+        # Get weather data
+        filtered_data = get_data(place, days)
 
+        if option == "Temperature":
+            # Plot temperature data
+            temperatures = [dict["main"]["temp"] / 10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
+        if option == "Sky":
+            images = {"Clear": "images/clear.png",
+                      "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png",
+                      "Snow": "images/snow.png"}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=115)
+    except KeyError:
+        st.write("The given location does not exist...")
